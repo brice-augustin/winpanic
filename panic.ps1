e6f4966640314be13ddd823041ef59afc213ab1d
Brice <brice.augustin@u-pec.fr>
2018-11-26 20:30:13 +0100

    new fake process names (windows-like), new incidents (dns, login failure)

--- panic.ps1
+++ panic.ps1
@@ -1,4 +1,4 @@
-﻿function exec
+function exec
 {
 	# -NoNewWindow does not seem to work in PowerShell ISE
     Start-Process -FilePath $args[0] -ArgumentList "-File",$args[1] -WindowStyle Hidden
@@ -23,21 +23,36 @@ Trouvez ce fichier et l'application responsable du problème, puis détruisez-le
 # No evt in 2016 when IP address changes ...
 # 2012 : evt from iphlpsvc, at least
 $contexte += "Le PC ne peut plus se connecter à internet.
-Vous devriez jeter un coup d'oeil sur l'observateur d'évènements ..."
+Vous devriez jeter un coup d'oeil sur la configuration IP..."
 
-$prog_prefix = "C:"
-$prog_list = @("$prog_prefix\explorer2.exe", "$prog_prefix\svchost2.exe", "$prog_prefix\smss2.exe")
+# 5
+$contexte += "Le PC ne peut plus se connecter à internet.
+Vous devriez jeter un coup d'oeil sur la configuration IP..."
+
+# 6
+$contexte += "On soupçonne que des pirates mexicains tentent de s'introduire
+dans notre réseau. Trouvez quel compte est attaqué."
+
+# 7
+$contexte += "Les services secrets mexicains ont réussi à pénétrer notre réseau !
+Regardez l'observateur d'évènements pour déterminer ce qu'ils ont modifié."
+
+$prog_prefix = "C:\Windows\System128"
 $powershell_path = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+$prog_list = @("explorer32.exe", "svchost32.exe", "smss32.exe", "csrss32.exe", "iexplore32.exe",
+						"acrotray32.exe", "services32.exe", "spoolsv32.exe", "savscan32.exe", "ctfmon32.exe")
+
+New-Item -Path "$prog_prefix" -ItemType "directory"
 
 foreach ($prog in $prog_list)
 {
-    Copy-Item $powershell_path $prog
+    Copy-Item "$powershell_path" "$prog_prefix\$prog"
 }
 
 foreach ($defi in 1..4 | Sort-Object {Get-Random})
 {
     $i = Get-Random -Maximum $prog_list.Length
-    $prog = $prog_list[$i]
+    $prog = "$prog_prefix\$prog_list[$i]"
 
     switch ($defi)
     {
@@ -55,14 +70,36 @@ foreach ($defi in 1..4 | Sort-Object {Get-Random})
         }
         4
         {
-            $ifIndex = (Get-NetAdapter -Physical | Where-Object status -eq "Up").ifIndex
-            # Release DHCP; works on 2012; not on 2016 ?
-			#$lan = WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object Index -eq $ifIndex
-            #$lan.ReleaseDHCPLease() | out-Null
-			# Configure static IP instead
-			New-NetIPAddress -InterfaceIndex 14 -IPAddress "198.51.100.1" -PrefixLength 24
+					$ifIndex = (Get-NetAdapter -Physical | Where-Object status -eq "Up").ifIndex
+					# Release DHCP; works on 2012; not on 2016 ?
+					#$lan = WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object Index -eq $ifIndex
+					#$lan.ReleaseDHCPLease() | out-Null
+					# Configure static IP instead
+					New-NetIPAddress -InterfaceIndex 14 -IPAddress "198.51.100.1" -PrefixLength 24
         }
         5
+				{
+					$ifIndex = (Get-NetAdapter -Physical | Where-Object status -eq "Up").ifIndex
+					Set-DnsClientserveraddress -InterfaceIndex 17 -ServerAddresses ("8.8.8.8")
+				}
+				6
+				{
+					foreach ($i in 1..5)
+					{
+						$pass = ConvertTo-SecureString -AsPlainText "keysersoze" -force
+						$c = New-Object System.Management.Automation.PSCredential("hank", $pass)
+						try
+						{
+							Start-Process .\notepad.exe -Credential $c
+						}
+						catch
+						{
+						}
+
+						Start-Sleep 1
+					}
+				}
+				10
         {
             # Planificateur des tâches désactivé en mode sans échec !
             $action=New-ScheduledTaskAction -Execute "C:\Windows\System32\shutdown.exe /r /t 30"
@@ -83,4 +120,6 @@ foreach ($defi in 1..4 | Sort-Object {Get-Random})
     Write-Host -NoNewline "Quand le problème est réglé, appuyez sur Entrée pour passer au suivant ..."
 
     Read-Host
+
+		Start-Sleep 5
 }
